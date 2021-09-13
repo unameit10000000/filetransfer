49b772639fb45533518730fc1ef46903ae864c2f
LnrCdr <free4ms@gmail.com>
2021-09-13 23:41:34 +0200

    Update datastore.py

--- datastore.py
+++ datastore.py
@@ -13,8 +13,6 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(128), unique=True)
-    access_key = db.Column(db.String(128), unique=True)
-    access_key_secret = db.Column(db.String(128), unique=True)
 
     def __init__(self, username, password):
         self.username = username
